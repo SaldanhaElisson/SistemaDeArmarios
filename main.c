5b74c2427c5034c33550886d78198aa7ffad07f3
SaldanhaElisson <elissonsaldanha19@gmail.com>
2022-04-22 20:27:59 -0300

    fix to bugs

--- main.c
+++ main.c
@@ -15,7 +15,6 @@ void pegandoBit(unsigned char aleatorio,  unsigned char armarios){
 // verificando se Ta ocupado
 int taOcupado( unsigned char armarioElemento, unsigned char armarios, unsigned char aleatorio ){
     // não está ocupado
-    printf("entrei aqui \n");
      if(!(armarioElemento && 1)){
         // trocando o bit na posição correta caso a posição não esteja
         armarioElemento = 0b000000001;
@@ -40,20 +39,24 @@ int main(){
     unsigned char auxArmario;
     unsigned char aleatorio;
 
-    if(armarios == 255){
+
+    while (1)
+    {
+        if(armarios == 255){
         printf("Todos os armarios estão ocupado: \n");
         return 0;
-    }
+        }
   
-    // escolher armario aleatorioamente
-    // escolher um numero aleatorio entre 1 e 8;
-    srand((unsigned)time(NULL));
-    aleatorio = rand() % 7;
-    printf("%d \n", aleatorio);
+        // escolher armario aleatorioamente
+        // escolher um numero aleatorio entre 1 e 8;
+        srand((unsigned)time(NULL));
+        aleatorio = rand() % 7;
+        printf("%d \n", aleatorio);
 
-    // pegando o bit da posição sorteada
-    pegandoBit(aleatorio, armarios);
-
+        // pegando o bit da posição sorteada
+        pegandoBit(aleatorio, armarios);
+    }
+    
 
       
     return 0;
